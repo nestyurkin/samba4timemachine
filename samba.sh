428ac9d2e04b791845a14ce72dd92d425511b605
Nestyurkin Nikolay <nestyurkin@gmail.com>
2018-05-31 18:32:13 +0300

    add users

--- samba.sh
+++ samba.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# truncated code from dperson samba.sh
+
 user() { local name="$1" passwd="$2" id="${3:-""}" group="${4:-""}"
     [[ "$group" ]] && { grep -q "^$group:" /etc/group || addgroup "$group"; }
     grep -q "^$name:" /etc/passwd ||
@@ -7,11 +9,15 @@ user() { local name="$1" passwd="$2" id="${3:-""}" group="${4:-""}"
     echo -e "$passwd\n$passwd" | smbpasswd -s -a "$name"
 }
 
-
 [[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o smbuser
 [[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o users
 
-[[ "${USER:-""}" ]] && eval user $(sed 's/^/"/; s/$/"/; s/;/" "/g' <<< $USER)
+while getopts ":u" opt; do
+    case "$opt" in
+        u) eval user $(sed 's/^/"/; s/$/"/; s/;/" "/g' <<< $OPTARG) ;;
+    esac
+done
+shift $(( OPTIND - 1 ))
 
 ionice -c 3 nmbd -D
 ionice -c 3 smbd -FS --no-process-group </dev/null
